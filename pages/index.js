74d97c8bc766c9ff3e678b35c7a5fcbeadf90627
vishu <bansalvishu56@gmail.com>
2023-03-28 20:12:00 +0530

    Adding blog from external source3

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,48 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>Vishu's Blog</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          Hello, I'm Vishu Bansal, a full stack developer from India. Here you
+          can find the blogs written by me on various programming topics, and
+          some interesting facts, etc.
         </p>
+        <p>
+          You can follow me on{" "}
+          <a href="https://twitter.com/vbstellar">Twitter</a>.
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData &&
+            allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))}
+        </ul>
       </section>
     </Layout>
   );
